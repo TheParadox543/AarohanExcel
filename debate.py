8816f771f6a45c882da2a6c2ea92960783e793e7
Sam Alex Koshy <samuel.alex.koshy@gmail.com>
2022-08-24 12:24:53 +0530

    added documentation, added save functionality
    Python PEP 8 standard documentation added to make it look more like a project.

--- debate.py
+++ debate.py
@@ -1,3 +1,22 @@
+"""
+Program to help in Aarohan Debate
+This program has been made to make the Aarohan Debate go smoother.
+
+The program checks whether a participant has registered for the event,
+checks their status, creates a list of participants to be sent in the order 
+by which they have arrived to mini audi. It also allows for changes to be 
+done in the case of any mistakes in user entry. 
+
+The program integrates with Excel to save data as it happens. It logs
+the timings of arrivals and departures from the mini audi.
+
+Idea by : Venkataram S
+Author : Sam Alex Koshy
+Starting date : 2022/08/22 
+
+"""
+
+# Importing libraries.
 import sys
 import os
 from datetime import datetime
@@ -11,37 +30,49 @@ from openpyxl.worksheet.worksheet import Worksheet
 from openpyxl.workbook.workbook import Workbook
 
 def main():
+    """The main functioning of the code. Keeps the code running."""
+    # Load workbook.
     wb = load_workbook("debate.xlsx")
     ws = wb.active
     cmds = "Commands are: check <no>, add <no>, send [no], "
     cmds += "name <name>, list, change, cmds"
     try:
         with open("list.json", 'r') as file:
+            # Load list of members to be sent to panel room.
             participants = json.load(file)
     except:
         participants = []
     os.system("start EXCEL.EXE debate.xlsx")
     print(cmds)
+    # Keep code running until cmd quit entered.
     while(True):
-        cmd = input("\nEnter command:")
+        cmd = input("\nEnter command : ")
         if cmd.lower() == "quit":
-            close_window(wb, participants)
+            _save_wb(wb, participants, True)
         elif cmd.lower().startswith("check"):
-            check_num(ws, cmd)
+            _check_reg(ws, cmd)
         elif cmd.lower().startswith("add"):
-            participants = add(ws, participants, cmd)
+            participants = _add(ws, participants, cmd)
         elif cmd.lower().startswith("send"):
-            participants = send(ws, participants, cmd)
+            participants = _send(ws, participants, cmd)
         elif cmd.startswith("list"):
             print(participants)
         elif cmd.lower().startswith("name"):
-            check_name(ws, cmd.lower())
+            _check_name(ws, cmd.lower())
         elif cmd.startswith("change"):
-            change(ws)
+            _change(ws)
         elif cmd.startswith("cmd"):
             print(cmds)
+        elif cmd.startswith("save"):
+            _save_wb(wb, participants, False)
 
-def check_num(ws:Worksheet, word):
+def _check_reg(ws:Worksheet, word):
+    """Check the status of a participant on typing their register number.
+
+    This function prints the details of the participant if they are registered,
+    and their status for the debate. If they are not a participant, prompt
+    is given to check their name in the list.
+    """
     try:
        reg_no = int(re.split("check ", word)[1])
     except:
@@ -50,21 +81,25 @@ def check_num(ws:Worksheet, word):
     for cell in colB:
         if cell.value == reg_no:
             row:int = cell.row
+            msg = ""
             for value in ws[row]:
-                print(value.value)
+                msg += str(value.value) + " "
+            print(msg)
             if ws.cell(row=row, column=4).value!=None:
                 print("Already sent the participant")
             elif ws.cell(row=row, column=3).value!=None:
-                print("Already logged in")
+                print("Participant is waiting in mini audi.")
             else:
-                print(datetime.now())
+                print("Participant has not arrived yet.")
             return
-    print(reg_no, "not found.")
-    # name = input("Enter name:")
-    # name += "name "
-    # check_name(ws, name)
+    print(reg_no, "not found. Check if their name is there with name <name>.")
+    name = input("Enter name : ")
+    name = "name " + name
+    _check_name(ws, name)
 
-def check_name(ws:Worksheet, word:str):
+def _check_name(ws:Worksheet, word:str):
+    """Check if the name exists in the list. If it exists, display their 
+    details including row number."""
     try:
         name = re.split("name ", word)[1]
     except:
@@ -75,8 +110,7 @@ def check_name(ws:Worksheet, word:str):
         if (re.search(cell.value.lower(), name) 
                 or re.search(name, cell.value.lower())):
             row = cell.row
-            print("\n", row)
-            msg = ""
+            msg = f"{row} "
             for value in ws[row]:
                 msg += str(value.value) + " "
             print(msg)
@@ -84,11 +118,13 @@ def check_name(ws:Worksheet, word:str):
     if found == 0:
         print(name, "not found.")
 
-def add(ws:Worksheet, participants:list[int], word):
+def _add(ws:Worksheet, participants:list[int], word):
+    """Add participants to the waiting list when they enter the venue.
+    Also log the time when they enter."""
     try:
         reg_no = int(re.split("add", word)[1])
     except:
-        return
+        return participants
     for cell in ws['B']:
         if cell.value == reg_no:
             row = cell.row
@@ -106,10 +142,10 @@ def add(ws:Worksheet, participants:list[int], word):
             json.dump(participants, file)
     except:
         print("Couldn't save list")
-        return
+        return participants
     return participants
 
-def send(ws:Worksheet, participants:list[int], word:str):
+def _send(ws:Worksheet, participants:list[int], word:str):
     try:
         reg_no = word[4:]
     except IndexError:
@@ -144,21 +180,23 @@ def send(ws:Worksheet, participants:list[int], word:str):
             json.dump(participants, file)
     except:
         print("Couldn't save list")
-        return
+        return participants
     return participants
 
-def change(ws:Worksheet):
-    row = int(input("Enter row address: "))
-    col = int(input("Enter column address: "))
+def _change(ws:Worksheet):
+    row = int(input("Enter row address : "))
+    col = int(input("Enter column address : "))
     if col == 1:
-        val = input("Enter name: ")
+        val = input("Enter name : ")
     elif col == 2:
-        val = int(input("Enter reg no: "))
+        val = int(input("Enter reg no : "))
+    elif col == 5:
+        val = input("Enter class : ")
     ws.cell(row=row, column=col, value=val)
     for value in ws[row]:
         print(value.value)
 
-def close_window(wb:Workbook, participants:list[int]):
+def _save_wb(wb:Workbook, participants:list[int], close:bool):
     try:
         os.system("taskkill/im EXCEL.EXE ")
     except:
@@ -171,7 +209,10 @@ def close_window(wb:Workbook, participants:list[int]):
         return
     else:
         print("List saved")
-    sleep(5)
+    if close:
+        sleep(10)
+    # else: 
+    #     sleep(1)
     try:
         wb.save("debate.xlsx")
     except PermissionError:
@@ -179,7 +220,8 @@ def close_window(wb:Workbook, participants:list[int]):
     else:
         print("Excel sheet saved")
         os.system("start EXCEL.EXE debate.xlsx")
-        sys.exit()
+        if close:
+            sys.exit()
 
 if __name__ == "__main__":
     main()
