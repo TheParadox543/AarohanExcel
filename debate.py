1ec94b259c244c9d3bd887c1c10f56b96ab00e30
Sam Alex Koshy <samuel.alex.koshy@gmail.com>
2022-08-25 18:55:11 +0530

    added documentation
    Documentation to all functions added. Also a new function to save the file was created.

--- debate.py
+++ debate.py
@@ -23,8 +23,8 @@ from datetime import datetime
 from time import sleep
 
 import re
-import json
-import openpyxl
+# import json
+# import openpyxl
 from openpyxl import load_workbook
 from openpyxl.worksheet.worksheet import Worksheet
 from openpyxl.workbook.workbook import Workbook
@@ -51,7 +51,7 @@ def main():
         elif cmd.lower().startswith("name"):
             _check_name(worksheet, cmd.lower())
         elif cmd.startswith("change"):
-            _change_participant_details(worksheet)
+            _change_participant_details(workbook, worksheet)
         elif cmd.startswith("cmd"):
             print(cmds)
         elif cmd.startswith("save"):
@@ -71,14 +71,15 @@ def _check_reg_no(worksheet:Worksheet, word:str):
     - worksheet `Worksheet`:
         The worksheet with which to work with.
     - word `str`:
-        The string that contains the register number along with the 
-        command that needs to be cut out.
+        The string that contains the register number of the participant, 
+        along with the command word.
     """
     try:
        reg_no = int(re.split("check ", word)[1])
     except:
         return
     colB = worksheet['B']
+
     # Iterate through the column to find the register number
     for cell in colB:
         if cell.value == reg_no:
@@ -86,7 +87,7 @@ def _check_reg_no(worksheet:Worksheet, word:str):
             row:int = cell.row
             msg = ""
             for value in worksheet[row]:
-                msg += str(value.value) + " "
+                msg += f"{value.value}, "
             print(msg)
 
             # Print status of participant.
@@ -107,49 +108,67 @@ def _check_reg_no(worksheet:Worksheet, word:str):
 def _check_name(worksheet:Worksheet, word:str):
     """Check if the name exists in the list. If it exists, display their 
     details including row number.
-    
+
     This function prints the details of the participant if their name is found.
 
     Parameters
     ----------
-    worksheet: `Worksheet`
+    - worksheet `Worksheet`:
         The worksheet with which to work with.
-    word: `str`
-        The string that contains the name along with the 
-        command that needs to be cut out.
+    - word `str`:
+        The string that contains the name of the participant, 
+        along with the command word.
     """
     try:
         name = re.split("name ", word)[1]
     except:
         return
     colA = worksheet['A']
-    found = 0
+    found = False
+    deleted = False
 
     # Iterate through the column to find the name.
     for cell in colA:
         if cell.value is None:
-            return
-        if (re.search(cell.value.lower(), name) 
+            worksheet.delete_rows(cell.row, 1)
+            deleted = True
+        elif (re.search(cell.value.lower(), name) 
                 or re.search(name, cell.value.lower())):
             # Print the details of the user if found.
             row = cell.row
-            msg = f"{row} "
+            msg = f"{row}. "
             for value in worksheet[row]:
-                msg += str(value.value) + " "
+                msg += f"{value.value}, "
             print(msg)
-            found = 1
-    if found == 0:
+            found = True
+    if deleted:
+        print("Rows were deleted, try again.")
+    elif found:
         print(name, "not found.")
 
 def _add_participant(workbook:Workbook, worksheet:Worksheet, word:str):
-    """Log the time when participants enter. 
-    
-    This function prints the details of the user"""
+    """Logs the time when the participant enters. 
+
+    This function prints the details of the user, including whether they
+    have already been added or if they have been sent.
+
+    Parameters
+    ----------
+    - workbook `Workbook`:
+        The workbook which has to be saved.
+    - worksheet `Worksheet`:
+        The worksheet with which to work with.
+    - word `str`:
+        The string that contains the register number of the participant, 
+        along with the command word. 
+    """
     try:
         reg_no = int(re.split("add", word)[1])
     except:
         return
     found = 0
+
+    # Iterate through the column to find the register number
     for cell in ['B']:
         if cell.value == reg_no:
             row = cell.row
@@ -165,20 +184,33 @@ def _add_participant(workbook:Workbook, worksheet:Worksheet, word:str):
     # If participant is not found, send message.
     if found == 0:
         print(reg_no, "not found.")
-
-    try:
-        workbook.save("debate.xlsx")
-    except PermissionError:
-        print("Failed to save")
+    _auto_save_workbook(workbook)
 
 def _send_participant(workbook:Workbook, worksheet:Worksheet, word:str):
-    """"""
+    """Log the time the participant is sent to their panel room, along with
+    the room they are sent to.
+
+    This function prints the details of the participant after logging their
+    details.
+
+    Parameters
+    ----------
+    - workbook `Workbook`:
+        The workbook which has to be saved.
+    - worksheet `Worksheet`:
+        The worksheet with which to work with.
+    - word `str`:
+        The string that contains the register number of the participant, 
+        along with the command word. 
+    """
     try:
         values = re.findall("\d+", word)
         reg_no = int(values[0])
         panel_room = int(values[1])
     except:
         return
+
+    # Iterate through the column to find the register number
     for cell in worksheet['B']:
         if cell.value == reg_no:
             worksheet.cell(row=cell.row, column=4, value=datetime.now())
@@ -186,37 +218,62 @@ def _send_participant(workbook:Workbook, worksheet:Worksheet, word:str):
             for value in worksheet[cell.row]:
                 print(value.value)
             print("Sent", reg_no)
-    try:
-        workbook.save("debate.xlsx")
-    except PermissionError:
-        print("Failed to save")
+    _auto_save_workbook(workbook)
 
-def _change_participant_details(worksheet:Worksheet):
+def _change_participant_details(workbook:Workbook, worksheet:Worksheet):
+    """Change the details of any participant.
+    
+    The name, register number or class of a participant can be changed.
+
+    Parameters
+    ----------
+    - workbook `Workbook`:
+        The workbook which has to be saved.
+    - worksheet `Worksheet`:
+        The worksheet with which to work with.
+    """
+    # Accept location of the change.
     row = int(input("Enter row address : "))
     col = int(input("Enter column address : "))
+    # Validate the location, then make the change
     if col not in [1, 2, 5]:
+        print("Invalid column number. Aborting change")
         return
-    if col == 1:
+    elif col == 1:
         val = input("Enter name : ")
     elif col == 2:
         val = int(input("Enter reg no : "))
     elif col == 5:
         val = input("Enter class : ")
     worksheet.cell(row=row, column=col, value=val)
+    msg = f"{row}. "
     for value in worksheet[row]:
-        print(value.value)
+        msg += f"{value.value}, "
+    print(msg)
+    _auto_save_workbook(workbook)
 
 def _save_workbook(workbook:Workbook, close:bool):
+    """Save the workbook on demand.
+
+    This function closes all excel windows to ensure that the workbook
+    object gets saved to a file.
+
+    Parameters
+    ----------
+    - workbook `Workbook`:
+        The workbook which has to be saved.
+    - close `bool`:
+        Whether the program needs to be closed after saving.
+    """
+    # Close all files to ensure a save.
     try:
         os.system("taskkill/im EXCEL.EXE ")
     except:
         print("Already closed")
-    else:
-        print("List saved")
+    # Give some time to ensure all sheets are closed.
     if close:
         sleep(10)
-    # else: 
-    #     sleep(1)
+    # Try to save file.
     try:
         workbook.save("debate.xlsx")
     except PermissionError:
@@ -224,8 +281,22 @@ def _save_workbook(workbook:Workbook, close:bool):
     else:
         print("Excel sheet saved")
         os.system("start EXCEL.EXE debate.xlsx")
+        # Close the program.
         if close:
             sys.exit()
+
+def _auto_save_workbook(workbook:Workbook):
+    """Save the workbook when needed.
+
+    Parameters
+    ----------
+    - workbook `Workbook`:
+        The workbook which has to be saved.
+    """
+    try:
+        workbook.save("debate.xlsx")
+    except PermissionError:
+        print("Failed to save")
 
 if __name__ == "__main__":
     main()
